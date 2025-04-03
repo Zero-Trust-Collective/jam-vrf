b6667b3d2c98e6afad8e0095c593f45042812fc2
0trust3r <197563558+0trust3r@users.noreply.github.com>
2025-04-03 12:02:42 -0400

    brevity for cleanliness in tests

--- tests/test_ring_commitment.py
+++ tests/test_ring_commitment.py
@@ -4,26 +4,6 @@ from pyvrf import (
 )
 
 def test_ring_commitment():
-    """Test ring commitment generation with known input/output values."""
-    # public keys
-    public_keys = [
-        bytes.fromhex("5e465beb01dbafe160ce8216047f2155dd0569f058afd52dcea601025a8d161d"),
-        bytes.fromhex("3d5e5a51aab2b048f8686ecd79712a80e3265a114cc73f14bdb2a59233fb66d0"),
-        bytes.fromhex("aa2b95f7572875b0d0f186552ae745ba8222fc0b5bd456554bfe51c68938f8bc"),
-        bytes.fromhex("7f6190116d118d643a98878e294ccf62b509e214299931aad8ff9764181a4e33"),
-        bytes.fromhex("48e5fcdce10e0b64ec4eebd0d9211c7bac2f27ce54bca6f7776ff6fee86ab3e3"),
-        bytes.fromhex("f16e5352840afb47e206b5c89f560f2611835855cf2e6ebad1acc9520a72591d")
-    ]
-
-    # expected output commitment
-    expected_commitment = bytes.fromhex("85f9095f4abd040839d793d89ab5ff25c61e50c844ab6765e2c0b22373b5a8f6fbe5fc0cd61fdde580b3d44fe1be127197e33b91960b10d2c6fc75aec03f36e16c2a8204961097dbc2c5ba7655543385399cc9ef08bf2e520ccf3b0a7569d88492e630ae2b14e758ab0960e372172203f4c9a41777dadd529971d7ab9d23ab29fe0e9c85ec450505dde7f5ac038274cf")
-
-    result = get_ring_commitment(public_keys)
-    
-    assert result == expected_commitment, f"Expected {expected_commitment.hex()}, but got {result.hex()}"
-
-def test_ring_commitment_eight_keys():
-    """Test ring commitment generation with a known 8-key ring."""
     # test vector sourced from: https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ring.json#L123
     # public keys
     public_keys = [
--- tests/test_single_signature.py
+++ tests/test_single_signature.py
@@ -4,15 +4,6 @@ from pyvrf import (
 
 def test_single_signature_verification():
     """ietf bandersnatch vrf signature verification"""
-    # vector 1
-    # https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ietf.json#L3
-    public_key = bytes.fromhex("a1b1da71cc4682e159b7da23050d8b6261eb11a3247c89b07ef56ccd002fd38b")
-    data = bytes.fromhex("")
-    ad = bytes.fromhex("")
-    output = VRFOutput(bytes.fromhex("e7aa5154103450f0a0525a36a441f827296ee489ef30ed8787cff8df1bef223f"))
-    proof = SingleVRFProof(bytes.fromhex("439fd9495643314fa623f2581f4b3d7d6037394468084f4ad7d8031479d9d101828bedd2ad95380b11f67a05ea0a76f0c3fef2bee9f043f4dffdddde09f55c01"))
-    SingleVRFVerifier().verify(public_key, data, ad, output, proof)
-
     # vector 7
     # https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ietf.json#L81
     public_key = bytes.fromhex("b0e1f208f9d6e5b310b92014ea7ef3011e649dab038804759f3766e01029d623")
