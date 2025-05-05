85e6004212a779553aca20badeaa0fd455442791
0trust3r <197563558+0trust3r@users.noreply.github.com>
2025-05-05 09:46:59 -0400

    refine mock data structure

--- python/tests/test_ietf.py
+++ python/tests/test_ietf.py
@@ -4,32 +4,29 @@ import json
 
 
 def test_valid_ietf_sig():
-    # vector 7 from https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ietf.json#L81
-
     # load mock data
     with open("mocks.json", "r") as f:
-        mock = json.load(f)["ietf"]["valid_signature"]
+        mock = json.load(f)["ietf"]
 
+    # verify signature
     ietf_verify(
         bytes.fromhex(mock["public_key"]),
         bytes.fromhex(mock["data"]),
         bytes.fromhex(mock["additional_data"]),
-        bytes.fromhex(mock["signature"]),
+        bytes.fromhex(mock["valid_signature"]),
     )
 
 
 def test_invalid_ietf_sig():
-    # vector 7 from https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ietf.json#L81
-    # invalid signature sourced from vector 6: https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ietf.json#L75-L78C17
-
     # load mock data
     with open("mocks.json", "r") as f:
-        mock = json.load(f)["ietf"]["invalid_signature"]
+        mock = json.load(f)["ietf"]
 
+    # verify signature
     with pytest.raises(ValueError, match="VRF verification failed"):
         ietf_verify(
             bytes.fromhex(mock["public_key"]),
             bytes.fromhex(mock["data"]),
             bytes.fromhex(mock["additional_data"]),
-            bytes.fromhex(mock["signature"]),
+            bytes.fromhex(mock["invalid_signature"]),
         )
--- python/tests/test_ring.py
+++ python/tests/test_ring.py
@@ -4,10 +4,9 @@ import json
 
 
 def test_ring_commitment():
-    # test vector sourced from: https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ring.json#L123
     # load mock data
     with open("mocks.json", "r") as f:
-        mock = json.load(f)["ring"]["valid_commitment"]
+        mock = json.load(f)["ring"]["commitment"]
 
     # compose list of public key bytes
     public_keys = []
@@ -18,19 +17,13 @@ def test_ring_commitment():
     commitment = get_ring_commitment(public_keys)
 
     # verify commitment
-    assert commitment == bytes.fromhex(mock["commitment"])
+    assert commitment == bytes.fromhex(mock["expected_commitment"])
 
 
 def test_valid_ring_sig():
-    """
-    Verify ring VRF signature of a valid jam ticket.
-
-    testvector sourced from: https://github.com/davxy/jam-test-vectors/blob/polkajam-vectors/safrole/tiny/publish-tickets-no-mark-6.json
-    """
-
     # load mock data
     with open("mocks.json", "r") as f:
-        mock = json.load(f)["ring"]["valid_signature"]
+        mock = json.load(f)["ring"]["safrole_ticket"]
 
     # construct ring verifier
     verifier = RingVerifier(bytes.fromhex(mock["root"]), mock["ring_size"])
@@ -40,12 +33,12 @@ def test_valid_ring_sig():
         (
             b"jam_ticket_seal" + bytes.fromhex(mock["entropy"]) + bytes([mock["attempt"]]),
             b"",
-            bytes.fromhex(mock["signature"]),
+            bytes.fromhex(mock["valid_signature"]),
         ),
         (
             b"jam_ticket_seal" + bytes.fromhex(mock["entropy"]) + bytes([mock["attempt"]]),
             b"",
-            bytes.fromhex(mock["signature"]),
+            bytes.fromhex(mock["valid_signature"]),
         ),
     ]
 
@@ -54,38 +47,29 @@ def test_valid_ring_sig():
 
 
 def test_invalid_ring_sig():
-    """
-    Verify ring VRF signature of an invalid jam ticket.
-
-    valid mock signature data sourced from: https://github.com/davxy/jam-test-vectors/blob/polkajam-vectors/safrole/tiny/publish-tickets-no-mark-6.json
-    invalid mock signature data sourced from: https://github.com/davxy/jam-test-vectors/blob/25aaedc8abc8c617287d9df56c916794db82eb92/safrole/tiny/publish-tickets-no-mark-3.json#L8
-    """
-
     # load mock data
     with open("mocks.json", "r") as f:
-        json_data = json.load(f)
-        valid_mock = json_data["ring"]["valid_signature"]
-        invalid_mock = json_data["ring"]["invalid_signature"]
+        mock = json.load(f)["ring"]["safrole_ticket"]
 
     # construct ring verifier
-    verifier = RingVerifier(bytes.fromhex(valid_mock["root"]), valid_mock["ring_size"])
+    verifier = RingVerifier(bytes.fromhex(mock["root"]), mock["ring_size"])
 
     # generate signatures
     signatures = [
         (
-            b"jam_ticket_seal" + bytes.fromhex(valid_mock["entropy"]) + bytes([valid_mock["attempt"]]),
+            b"jam_ticket_seal" + bytes.fromhex(mock["entropy"]) + bytes([mock["attempt"]]),
             b"",
-            bytes.fromhex(valid_mock["signature"]),
+            bytes.fromhex(mock["valid_signature"]),
         ),
         (
-            b"jam_ticket_seal" + bytes.fromhex(invalid_mock["entropy"]) + bytes([invalid_mock["attempt"]]),
+            b"jam_ticket_seal" + bytes.fromhex(mock["entropy"]) + bytes([mock["attempt"]]),
             b"",
-            bytes.fromhex(invalid_mock["signature"]),
+            bytes.fromhex(mock["invalid_signature"]),
         ),
         (
-            b"jam_ticket_seal" + bytes.fromhex(invalid_mock["entropy"]) + bytes([invalid_mock["attempt"]]),
+            b"jam_ticket_seal" + bytes.fromhex(mock["entropy"]) + bytes([mock["attempt"]]),
             b"",
-            bytes.fromhex(invalid_mock["signature"]),
+            bytes.fromhex(mock["invalid_signature"]),
         ),
     ]
 
