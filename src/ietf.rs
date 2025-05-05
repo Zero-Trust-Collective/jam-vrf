ca066a1e48b5cbde00e5c988b3b0048d28446b30
0trust3r <197563558+0trust3r@users.noreply.github.com>
2025-05-05 10:54:02 -0400

    update rust tests to source from mocks.json

--- src/ietf.rs
+++ src/ietf.rs
@@ -55,24 +55,57 @@ pub fn ietf_verify(public_key: &[u8], data: &[u8], ad: &[u8], signature: &[u8]) 
 #[cfg(test)]
 mod tests {
     use super::ietf_verify;
+    use serde;
+    use serde::Deserialize;
+    use serde_json::Value;
+    use serde_with::hex::Hex;
+    use serde_with::serde_as;
+    use std::fs::File;
+    use std::io::BufReader;
+
+    #[serde_as]
+    #[derive(Deserialize)]
+    pub struct IETF {
+        #[serde_as(as = "Hex")]
+        pub public_key: Vec<u8>,
+        #[serde_as(as = "Hex")]
+        pub data: Vec<u8>,
+        #[serde_as(as = "Hex")]
+        pub additional_data: Vec<u8>,
+        #[serde_as(as = "Hex")]
+        pub valid_signature: Vec<u8>,
+        #[serde_as(as = "Hex")]
+        pub invalid_signature: Vec<u8>,
+    }
 
     #[test]
-    fn test_ietf_verify() {
-        // vector 7 from https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ietf.json#L81
-        let public_key =
-            hex::decode("b0e1f208f9d6e5b310b92014ea7ef3011e649dab038804759f3766e01029d623")
-                .unwrap();
-        let data = hex::decode("42616e646572736e6174636820766563746f72").unwrap();
-        let ad = hex::decode("1f42").unwrap();
-        let signature = hex::decode("6d1dd583bea262323c7dc9e94e57a472e09874e435719010eeafae503c433f166dbeeab9648505fa6a95de52d611acfbb2febacc58cdc7d0ca45abd8c952ef12ce7f4a2354a6c3f97aee6cc60c6aa4c4430b12ed0f0ef304b326c776618d7609")
-                .unwrap();
+    fn test_signature_verification() {
+        // load mock data
+        let file = File::open("mocks.json").unwrap();
+        let reader = BufReader::new(file);
+        let mock_json: Value = serde_json::from_reader(reader).unwrap();
+        let mock: IETF = serde_json::from_value(mock_json["ietf"].clone()).unwrap();
 
         // verify valid signature
-        ietf_verify(&public_key, &data, &ad, &signature)
-            .expect("signature verification should pass");
+        ietf_verify(
+            &mock.public_key,
+            &mock.data,
+            &mock.additional_data,
+            &mock.valid_signature,
+        )
+        .expect("signature verification should pass");
 
-        // verify invalid signature
-        let signature = hex::decode("9508104b820469687488d83f729288d9f70fc0523318beff44a47da10d490b3c4fa53519bd9d17acae4d1021416557d11b84dd4670b563770c14eb98161eaa080f7f9bee9077427f547e69b919cf8d63823c14b20085fd9516768e0f5e3d3f0e").unwrap();
-        assert!(ietf_verify(&public_key, &data, &ad, &signature).is_err());
+        // verify ininvalid signature
+        let result = ietf_verify(
+            &mock.public_key,
+            &mock.data,
+            &mock.additional_data,
+            &mock.invalid_signature,
+        );
+        assert!(result.is_err());
+        assert_eq!(
+            result.unwrap_err().to_string(),
+            "ValueError: VRF verification failed"
+        );
     }
 }
--- src/ring.rs
+++ src/ring.rs
@@ -203,72 +203,117 @@ impl RingVerifier {
 #[cfg(test)]
 mod tests {
     use super::{get_ring_commitment, RingVerifier};
+    use pyo3::prelude::*;
     use pyo3::Python;
+    use serde;
+    use serde::Deserialize;
+    use serde_json::Value;
+    use serde_with::hex::Hex;
+    use serde_with::serde_as;
+    use std::fs::File;
+    use std::io::BufReader;
+
+    #[serde_as]
+    #[derive(Deserialize)]
+    pub struct SafroleTicket {
+        pub attempt: u8,
+        #[serde_as(as = "Hex")]
+        pub root: Vec<u8>,
+        #[serde_as(as = "Hex")]
+        pub entropy: Vec<u8>,
+        pub ring_size: usize,
+        #[serde_as(as = "Hex")]
+        pub valid_signature: Vec<u8>,
+        #[serde_as(as = "Hex")]
+        pub invalid_signature: Vec<u8>,
+    }
+
+    #[serde_as]
+    #[derive(Deserialize)]
+    pub struct COMMITMENT {
+        #[serde_as(as = "Vec<Hex>")]
+        pub keys: Vec<Vec<u8>>,
+        #[serde_as(as = "Hex")]
+        pub expected_commitment: Vec<u8>,
+    }
 
     #[test]
-    fn test_ring_commitment() {
-        // test vector sourced from: https://github.com/davxy/bandersnatch-vrf-spec/blob/6b1ceba5b3cbc834201732bcdad1377e19e9283e/assets/vectors/bandersnatch_sha-512_ell2_ring.json#L123
-        let public_keys = vec![
-            hex::decode("7b32d917d5aa771d493c47b0e096886827cd056c82dbdba19e60baa8b2c60313")
-                .unwrap(),
-            hex::decode("d3b1bdb321123449c6e89d310bc6b7f654315eb471c84778353ce08b951ad471")
-                .unwrap(),
-            hex::decode("561fdb0dcfb8bd443718b942f82fe717238cbcf8d12b8d22861c8a09a984a3c5")
-                .unwrap(),
-            hex::decode("b0e1f208f9d6e5b310b92014ea7ef3011e649dab038804759f3766e01029d623")
-                .unwrap(),
-            hex::decode("4fd11f89c2a1aaefe856bb1c5d4a1fad73f4de5e41804ca2c17ba26d6e10050c")
-                .unwrap(),
-            hex::decode("86d06ee2c70da6cf2da2a828d8a9d8ef755ad6e580e838359a10accb086ae437")
-                .unwrap(),
-            hex::decode("ad6fdeda0dde0a57c51d3226b87e3795e6474393772da46101fd597fbd456c1b")
-                .unwrap(),
-            hex::decode("3f9dc0c4f67f207974123830c2d66988fb3fb44becbbba5a64143f376edc51d9")
-                .unwrap(),
-        ];
+    fn test_commitment_generation() {
+        // load mock data
+        let file = File::open("mocks.json").unwrap();
+        let reader = BufReader::new(file);
+        let mock_json: Value = serde_json::from_reader(reader).unwrap();
+        let mock: COMMITMENT =
+            serde_json::from_value(mock_json["ring"]["commitment"].clone()).unwrap();
 
-        // Expected output commitment
-        let expected_ring = hex::decode("a359e70e307799b111ad89b162b4260fb4a96ebf4232e8b02d396033498d4216305ed9cff3584a4c68f03ab3df87243a80bfc965633efc23c82ca064afe105baacccbf23e47b543d16c3c4466a83242a77acc16f79b8710051b5e97c85319cf392e630ae2b14e758ab0960e372172203f4c9a41777dadd529971d7ab9d23ab29fe0e9c85ec450505dde7f5ac038274cf").unwrap();
-
-        // Generate root
+        // generate commitment
         Python::with_gil(|py| {
-            let commitment = get_ring_commitment(py, public_keys).unwrap();
-            // Verify results
-            assert_eq!(commitment.as_bytes(py), expected_ring.as_slice());
+            let commitment = get_ring_commitment(py, mock.keys).unwrap();
+            // Verify commitment
+            assert_eq!(commitment.as_bytes(py), mock.expected_commitment.as_slice());
         });
     }
 
     #[test]
-    fn test_ring_vrf_verify() {
-        // Verify a batch of ring VRF signatures
-        // testvector sourced from: https://github.com/davxy/jam-test-vectors/blob/polkajam-vectors/safrole/tiny/publish-tickets-no-mark-6.json
-        let signature = hex::decode("1dfb7b61deee0c4a6899c1123e9e362f2b965079be576aebda0b7ac5e111186ec11372ea37a823a74a10a7d0c3b9b850d07ea9367f7560d0271a34125983fb0a4acb929b0817c4e3710915ea07e64d72198cbb2399e318940569d4c80ead4b5b39fa798e7a8327dcba940ffa77659b1850f877be7a439fc3a66191299d5ae92492d1a27a97455ca697913b3d7c7a9f95bfe58bda53a2641e4a0752f4efbb1a0417d77dedf11d514684f94b0729672a5808088b5445ceb540db00aa8934faa314a445e67a96d618c4fc1c100a69fa449cca1db6a7ac609d7bd04107f685411aa1bebecdb3897ed0d6c00b46a56381e5968b7520b215677afa1394464057709837dfb22b04b53c69011da7926c341cf6e77a2ed27912c40267c826cca53f876dad8a11740bbf7fec24eacf37246038a6da9d4fa923e40efb6d0a136ff2349fc1c6a1f0d199e87022d71cb503b9ea45a774988b0a3abe6f2a8bbeade7e72f14a27fd1fd2c34bf751a4397df18d16cea6ae40282ce51c36d85f5a90896070bcca248ecce27455c091c0f2e455ea83aaeac9b3c2df6cac95f34f7bd086746a414a3414f48f24f725ea802bc92a56a089f5af93122f85f7474380f3a3534553663ff34ea66f50ce876aa4bcec33ca5e7d0b16677c190d5c8365b3d55c3873b634ca53e988ca8860b97577f3b0265584b0544dde70556b35b86118c7b7c94232fb8fc30b6f407647ce39e40d0769e8a5d906f17a2ead9ab49ba3b3e669b3d48017ebf340e00a03ad82ad420ac3bc63b5f06cb15a0a938848a382ddaebabfe7b3ec13270f6c3e4c14aca85963f879bfc5937244bad613b0ed92f86205874a639fe758f3ab5c69222ea2460792c9c42732c74716886b2e8f9a155e5d439246bff0b21baf74f8e7b672c61f1328939f03be4e5d4dffa6f2d8231734a0ed8e82fef9dd00cb1ab1dfe37f0ddb12e1861570b6e4cab1ab80e774048227493bb8bad60df218b2a08e6d2230f1d040ce47e2d473d0e925b2a91abcb7f867ee2f41ad64409aa81a0937f130ebdf53e6ee61480c73708af790d1134b7a0b472573e1008b9c9d0ee95d40a3909d94e6cf0044d916ff9461844").unwrap();
-        let ring_root = hex::decode("85f9095f4abd040839d793d89ab5ff25c61e50c844ab6765e2c0b22373b5a8f6fbe5fc0cd61fdde580b3d44fe1be127197e33b91960b10d2c6fc75aec03f36e16c2a8204961097dbc2c5ba7655543385399cc9ef08bf2e520ccf3b0a7569d88492e630ae2b14e758ab0960e372172203f4c9a41777dadd529971d7ab9d23ab29fe0e9c85ec450505dde7f5ac038274cf").unwrap();
-        let entropy: Vec<u8> =
-            hex::decode("bb30a42c1e62f0afda5f0a4e8a562f7a13a24cea00ee81917b86b89e801314aa")
-                .unwrap();
-        let ring_size = 6;
+    fn test_signature_verification() {
+        // load mock data
+        let file = File::open("mocks.json").unwrap();
+        let reader = BufReader::new(file);
+        let mock_json: Value = serde_json::from_reader(reader).unwrap();
+        let mock: SafroleTicket =
+            serde_json::from_value(mock_json["ring"]["safrole_ticket"].clone()).unwrap();
 
         // construct ring verifier
-        let verifier = RingVerifier::new(&ring_root, ring_size).unwrap();
+        let verifier = RingVerifier::new(&mock.root, mock.ring_size).unwrap();
 
-        // verify batch of valid signatures
+        // generate batch of valid signatures
         let mut data = Vec::new();
         data.extend_from_slice(b"jam_ticket_seal");
-        data.extend_from_slice(entropy.as_slice());
-        data.push(1);
+        data.extend_from_slice(mock.entropy.as_slice());
+        data.push(mock.attempt);
         let ad = b"";
-        let batch_len = 5;
         let mut batch = vec![];
-        for _ in 0..batch_len {
-            batch.push((data.to_vec(), ad.to_vec(), signature.to_vec()));
+        for _ in 0..2 {
+            batch.push((data.to_vec(), ad.to_vec(), mock.valid_signature.to_vec()));
         }
+
+        // verify valid signatures
         verifier
             .verify(batch.clone())
             .expect("signature verification should pass");
 
-        // verify batch that contains an invalid signature
-        batch.push((data.to_vec(), b"bad_ad".to_vec(), signature.to_vec()));
-        assert!(verifier.verify(batch).is_err());
+        // append a few bad signatures to our batch
+        batch.push((
+            data.to_vec(),
+            b"bad_ad".to_vec(),
+            mock.invalid_signature.to_vec(),
+        ));
+        batch.push((
+            data.to_vec(),
+            b"bad_ad".to_vec(),
+            mock.invalid_signature.to_vec(),
+        ));
+
+        // verify batch that contains invalid signatures
+        let result = verifier.verify(batch);
+        // signature verification should raise an error
+        assert!(result.is_err());
+        // verify the error contains a dict identifying each of the invalid signatures
+        Python::with_gil(|py| {
+            let invalid_signatures = result
+                .unwrap_err()
+                .value(py)
+                .getattr("args")
+                .unwrap()
+                .get_item(0)
+                .unwrap();
+            assert_eq!(invalid_signatures.len().unwrap(), 2);
+
+            let invalid_batch_indices = [2, 3];
+            for i in invalid_batch_indices {
+                let err = invalid_signatures.get_item(i).unwrap().str().unwrap();
+                assert_eq!(err, "VRF verification failed");
+            }
+        })
     }
 }
